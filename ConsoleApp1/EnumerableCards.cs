ff018b0e67bbf23c1886841bdf01e18189d9e43d
pKuEDhVxaGFYBTRz <90396737+pKuEDhVxaGFYBTRz@users.noreply.github.com>
2021-11-19 10:08:11 +0000

    Add files via upload

---
+++ ConsoleApp1/EnumerableCards.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complex_Pair_Program
+{
+    class CardPointers
+    {
+        public enum EnumerableCards
+        {
+            gold1,
+            gold2,
+            gold3,
+            points1,
+            points2,
+            points3,
+        }
+
+        private object cardClass = new Dictionary<object, Card>()
+        {
+            { EnumerableCards.gold1, },
+            { EnumerableCards.gold2, }
+        };
+
+        public object CardClass { get => cardClass; set => cardClass = value; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Complex_Pair_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // main program
+
+        }
+    }
+}
