9132c90663b2733dfe78e572ff23b6ef56d08d56
AzQth <60754904+AzQth@users.noreply.github.com>
2021-11-19 10:13:50 +0000

    Add files via upload

---
+++ CellarCard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complex_Pair_Program
+{
+    class CellarCard:Card
+    {
+        public CellarCard(string n, int c) : base(n, c)
+        {
+
+        }
+
+        public static List<Card> Perform(List<Card> hand)
+        {
+            //Displays cards to the user
+            Console.WriteLine("Hand:");
+            foreach(Card card in hand)
+            {
+                Console.WriteLine(card.Name);
+            }
+
+            //Allows user to remove cards
+            Console.WriteLine("Please type the name of any cards that you wish to remove. Once you are done, type 'Done'.");
+
+            while (true)
+            {
+                //Exits the loop if the hand is empty.
+                if(hand.Count <= 0)
+                {
+                    Console.WriteLine("Hand empty.");
+                    break;
+                }
+
+                string cardToRemove = Console.ReadLine();
+
+                //Exits the loop if the user entered 'Done'.
+                if(cardToRemove.ToLower() == "done")
+                {
+                    break;
+                }
+                
+                //Removes a card with the same name as the entered one.
+                for(int i = hand.Count; i >= 0; i++)
+                {
+                    if(hand[i].Name == cardToRemove)
+                    {
+                        hand.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+
+            //Returns remaining cards
+            return hand;
+        }
+    }
+}
